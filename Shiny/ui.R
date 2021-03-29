965c54d1fc34f234570c8adc4fed9fc0ea23c1e2
d <dvedensky@gmail.com>
2021-03-28 20:30:03 -0500

    Fix labelling and reset inactivity timer to its original value

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -12,7 +12,6 @@ employ_min <- 0
 receipt_max <- 400
 receipt_min <- 0
 
-features <- c("FRANCHISE","RACE_ETH","SECTOR","SEX","VET")
 
 inactivity <- "function idleTimer() {
   var t = setTimeout(logout, 60000);
@@ -28,7 +27,7 @@ inactivity <- "function idleTimer() {
 
   function resetTimer() {
     clearTimeout(t);
-    t = setTimeout(logout, 600000);  // time is in milliseconds (1000 is 1 second)
+    t = setTimeout(logout, 60000);  // time is in milliseconds (1000 is 1 second)
   }
 }
 idleTimer();"
@@ -37,11 +36,7 @@ fluidPage(
   tags$script(inactivity),
   tags$h1("ELM Predictions"),
   sidebarLayout(
-    #Discrete variables on the left, 
-    #plot with two numeric variables on the axes and posterior mean surface
-    #Below that, user selects a value for the two numeric variables
-    #second plot shows posterior predictive distribution for all inputs
-    sidebarPanel(tags$h2("Features"),
+    sidebarPanel(tags$h3("Categorical variables"),
                 selectInput("franchise", "Franchise", levs$FRANCHISE),
                 selectInput("race_eth", "Race", levs$RACE_ETH),
                 selectInput("sector", "Sector", levs$SECTOR),
@@ -52,20 +47,26 @@ fluidPage(
 			    tabPanel("Mean predictive surface",
                                     fluidRow(
                                     plotlyOutput("surface"),
-                                       column(3, offset=2, sliderInput("receipts", "Establishment Receipts", 
-                                       	     min=0, max=receipt_max, value=100)),
-                                       column(3, offset=1, sliderInput("employment", "Establishment employment", 
-                                       	     min=0, max=employ_max, value=10)))),
-			    tabPanel("Posterior predictive...",
+				    )),
+			    tabPanel("Posterior pred. density",
 				    br(),
                                     plotOutput("density")
 			            ),
-			    tabPanel("All factor levels",
+			    tabPanel("Factor violin levels",
 				    br(),
-				    selectInput("factor", "Variable to facet", features),
+				    selectInput("factor", "Variable to facet", list("Franchise"="FRANCHISE",
+										    "Race/Ethnicity"="RACE_ETH",
+										    "Sector"="SECTOR",
+										    "Sex"="SEX",
+										    "Veteran"="VET")),
 			           plotOutput("facet_densities")
 			    )
-		           )
+		           ),
+	                   br(),
+                           fluidRow(column(3, offset=2, sliderInput("receipts", "Receipts (in $1000s)", 
+                                       	     min=0, max=receipt_max, value=100)),
+                                       column(3, offset=1, sliderInput("employment", "Number employees", 
+                                       	     min=0, max=employ_max, value=10))))
              )
        )
-  )
+  
